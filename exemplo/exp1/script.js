fc6864fbbde51e54f6ef13bcbf3679287a2af282
s7lima <bruno.eslima@senacsp.edu.br>
2024-10-09 20:20:00 -0300

    Feat : exp2 corrigido

--- exemplo/exp1/script.js
+++ exemplo/exp1/script.js
@@ -11,21 +11,49 @@
 
 // console.log(vetor);
 
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
 // *Crie um algoritimo que leia um vetor de 5 números inteiros e mostre-os *//
 
-let numeros =[]
+// let numeros =[]
 
-for (let i = 0; i < 5; i++) {
+// for (let i = 0; i < 5; i++) {
 
-    let numero = prompt(`Insira o ${i + 1}° número`);
-    while (isNaN(numero)) {
-        alert("Isso não é um número valido")
-        numero = prompt("Insira um número novamente!")
+//     let numero = prompt(`Insira o ${i + 1}° número`);
+//     while (isNaN(numero)) {
+//         alert("Isso não é um número valido")
+//         numero = prompt("Insira um número novamente!")
+//     }
+//     numero = parseInt(numero)
+//     // Inserido o número digitado ao vetor
+//     numeros.push(numero)
+
+// }
+
+// console.log(numeros);
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Crie um algoritiomo que leia 4 notas, mostre as notas e a média na tela
+
+let notas = [];
+for (let i = 0; i < 4; i++) {
+    let nota = prompt(`Informe a ${i + 1}° nota:`);
+    nota = nota.replace(',', '.');
+    while (isNaN(nota) || nota.trim() === "") {
+        alert("Nota Invalida!");
+        nota = prompt(`Informe a ${i + 1}° nota novamete:`)
     }
-    numero = parseInt(numero)
-    // Inserido o número digitado ao vetor
-    numeros.push(numero)
-
+    nota = parseFloat(nota);
+    // notas.push(nota);
+    notas[i] = nota;
 }
+let soma = 0;
+let media = 0;
+let quant = notas.length
+for (let j = 0; j <quant; j++) {
+    soma += notas[j];
+}
+media = soma/quant
 
-console.log(numeros);
+alert(`A média das notas informadas é: ${media.toFixed(2)}`)
